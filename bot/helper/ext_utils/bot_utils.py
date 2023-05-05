f8508e43ab6a53391a0ea10486808e50c9098787
dasaraul <acmadauliai@gmail.com>
2023-05-05 11:40:15 +0700

    edit lagi

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -372,7 +372,7 @@ def get_readable_message():
         buttons = ButtonMaker()
         buttons.sbutton("Refresh", "status refresh")
         buttons.sbutton("Statistics", str(THREE))
-        buttons.buildbutton(f"Repo", f"https://github.com/SN-Abdullah-Al-Noman/SN_WZML")
+        buttons.buildbutton(f"Donate", f"https://telegra.ph/file/d6b1a09ba8dc97cdc018d.jpg")
         buttons.sbutton("Close", "status close")
         sbutton = buttons.build_menu(2)
         
@@ -382,14 +382,14 @@ def get_readable_message():
             buttons.sbutton(f"{PAGE_NO}/{PAGES}", str(THREE))
             buttons.sbutton("Next⏩", "status nex")
             buttons.sbutton("Refresh", "status refresh")
-            buttons.buildbutton(f"Repo", f"https://github.com/SN-Abdullah-Al-Noman/SN_WZML")
+            buttons.buildbutton(f"Donate", f"https://telegra.ph/file/d6b1a09ba8dc97cdc018d.jpg")
             buttons.sbutton("Close", "status close")
         else:
             buttons.sbutton("Previous", "status pre")
             buttons.sbutton(f"{PAGE_NO}/{PAGES}", str(THREE))
             buttons.sbutton("Next", "status nex")
             buttons.sbutton("Refresh", "status refresh")
-            buttons.buildbutton(f"Repo", f"https://github.com/SN-Abdullah-Al-Noman/SN_WZML")
+            buttons.buildbutton(f"Donate", f"https://telegra.ph/file/d6b1a09ba8dc97cdc018d.jpg")
             buttons.sbutton("Close", "status close")
             button = buttons.build_menu(3)
             return msg + bmsg, button
